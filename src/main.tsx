cf7b5e7427edb99a6cf0a9d152d19d85ea37a9d5
xue-ding-e-a <2543165843@qq.com>
2026-06-29 18:41:04 +0800

    fix: 使用 HashRouter 解决 GitHub Pages 路由问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { AuthProvider } from './store/AuthContext';
 import './styles/reset.css';
@@ -10,9 +10,9 @@ import './styles/global.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </React.StrictMode>,
 );
